eb35de7dea6fd929e6a85e3ea1db3c72f38e769e
Ainaz99 <ainaz.eftekhar@gmail.com>
2021-10-18 16:18:45 +0200

    changed kmeans method

--- KMeans.py
+++ KMeans.py
@@ -23,18 +23,26 @@ def run(docs):
         distance = list(x2 - 2 * xy + y2)
         for j in range(len(new_docs)):
             clusters[list(distance[j]).index(min(list(distance[j])))].append(j)
+            clusters[list(distance[j]).index(min(list(distance[j])))].append(j)
+
 
         for j in range(len(new_docs)):
             clusters[list(distance[j]).index(min(list(distance[j])))].append(j)
+            clusters[list(distance[j]).index(min(list(distance[j])))].append(j)
+
 
         for j in range(4):
             sum_of_vectors = [0 for _ in range(len(vocabulary))]
+            sum_of_vectors = [0 for _ in range(len(vocabulary))]
+
             for k in clusters[j]:
                 sum_of_vectors = numpy.add(sum_of_vectors, train_matrix[k])
             centroids[j] = numpy.asarray(sum_of_vectors / len(clusters[j]) if len(clusters[j]) > 0 else sum_of_vectors)
 
         for j in range(4):
             sum_of_vectors = [0 for _ in range(len(vocabulary))]
+            sum_of_vectors = [0 for _ in range(len(vocabulary))]
+
             for k in clusters[j]:
                 sum_of_vectors = numpy.add(sum_of_vectors, train_matrix[k])
             centroids[j] = numpy.asarray(sum_of_vectors / len(clusters[j]) if len(clusters[j]) > 0 else sum_of_vectors)
